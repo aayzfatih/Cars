cbb4f72ec9839281e82757ef5d6873f7f7d6a291
aayzfatih <fatihayaz076@gmail.com>
2023-09-28 13:14:13 +0300

    Project has been finished

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,15 +2,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
   };
   const renderingFunction = cars.map((car) => {
+    //Decide if this car should be bold
+    //state.form.name
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name}-${car.cost}
         </p>
