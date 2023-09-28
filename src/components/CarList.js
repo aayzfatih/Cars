ec2b68e4037be5964d79922e27b6e2d832178a24
aayzfatih <fatihayaz076@gmail.com>
2023-09-28 12:08:00 +0300

    adding and removing car operation added

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,4 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store";
 function CarList() {
-  return <div>Car List</div>;
+  const dispatch = useDispatch();
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+  const renderingFunction = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name}-${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return (
+    <div className="car-list">
+      {renderingFunction}
+      <hr />
+    </div>
+  );
 }
 export default CarList;
